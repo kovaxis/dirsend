108e9e60e9a42c9366114674ed7c7a9192929754
kovaxis <martin.andrighetti@gmail.com>
2025-02-03 21:28:11 -0300

    initial commit

---
+++ src/aes_gcm_1.rs
@@ -0,0 +1,282 @@
+use crate::prelude::*;
+
+use aes_gcm::{aead::AeadMutInPlace, Aes256Gcm, KeyInit};
+use argon2::Argon2;
+
+const AES_TAG_SIZE: usize = 16;
+const AES_KEY_SIZE: usize = 32;
+const U128_SIZE: usize = std::mem::size_of::<u128>();
+const PLAINTEXT_CHUNK_SIZE: usize = 4 * 1024;
+const SALT_SIZE: usize = 32;
+pub const CIPHERTEXT_CHUNK_SIZE: usize = PLAINTEXT_CHUNK_SIZE + AES_TAG_SIZE;
+
+fn init_aes_cipher(pass: &[u8], salt: [u8; 32]) -> Result<(Aes256Gcm, u128)> {
+    let mut key = [0u8; AES_KEY_SIZE];
+    Argon2::default()
+        .hash_password_into(pass, &salt[..], &mut key)
+        .map_err(|e| anyhow!("{}", e))
+        .context("key derivation error")?;
+    let cipher = Aes256Gcm::new(&key.into());
+    let mut nonce = [0u8; U128_SIZE];
+    nonce[..].copy_from_slice(&salt[..U128_SIZE]);
+    Ok((cipher, u128::from_le_bytes(nonce)))
+}
+
+pub struct AesEncrypt<R> {
+    inner: R,
+    cipher: Aes256Gcm,
+    nonce: u128,
+    last_chunk: bool,
+    buf: Box<[u8]>,
+    buf_len: usize,
+    buf_skip: usize,
+}
+impl<R: Read> AesEncrypt<R> {
+    pub fn new(pass: &[u8], inner: R) -> Result<Self> {
+        let salt = {
+            let mut buf = [0u8; SALT_SIZE];
+            getrandom::fill(&mut buf).map_err(|e| anyhow!("getrandom error: {}", e))?;
+            buf
+        };
+        let (cipher, nonce) = init_aes_cipher(pass, salt)?;
+
+        let mut buf = vec![0u8; usize::max(SALT_SIZE, CIPHERTEXT_CHUNK_SIZE)].into_boxed_slice();
+        buf[..SALT_SIZE].copy_from_slice(&salt[..]);
+        Ok(Self {
+            inner,
+            cipher,
+            nonce,
+            buf,
+            buf_len: SALT_SIZE,
+            buf_skip: 0,
+            last_chunk: false,
+        })
+    }
+
+    /// Expects a buffer of size at least `CIPHERTEXT_CHUNK_SIZE`.
+    fn fill_buf(&mut self) -> io::Result<usize> {
+        // Read plaintext into buffer
+        let mut filled = AES_TAG_SIZE;
+        while filled < CIPHERTEXT_CHUNK_SIZE {
+            let n = self
+                .inner
+                .read(&mut self.buf[filled..CIPHERTEXT_CHUNK_SIZE])?;
+            if n == 0 {
+                break;
+            }
+            filled += n;
+        }
+        eprintln!("read {} bytes of plaintext", filled - AES_TAG_SIZE);
+        if filled < CIPHERTEXT_CHUNK_SIZE {
+            self.last_chunk = true;
+        }
+        if filled == 0 {
+            return Ok(0);
+        }
+        // Get next nonce
+        let mut nonce = [0u8; 12];
+        nonce.copy_from_slice(&self.nonce.to_le_bytes()[..12]);
+        self.nonce = self.nonce.saturating_add(1);
+        // Encrypt buffer
+        let tag: [u8; AES_TAG_SIZE] = self
+            .cipher
+            .encrypt_in_place_detached(&nonce.into(), &[], &mut self.buf[AES_TAG_SIZE..filled])
+            .map_err(|_e| io::Error::new(io::ErrorKind::Other, "aes encryption error"))?
+            .into();
+        // Write tag into buffer
+        self.buf[..AES_TAG_SIZE].copy_from_slice(&tag[..]);
+        Ok(filled)
+    }
+}
+impl<R> Read for AesEncrypt<R>
+where
+    R: Read,
+{
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        // Refill buffer if it is empty
+        if self.buf_skip >= self.buf_len {
+            eprintln!("refilling encrypt buf");
+            // If we already reached the last chunk, short-circuit
+            if self.last_chunk {
+                return Ok(0);
+            }
+            // Fill the inner buffer
+            let filled = self.fill_buf()?;
+            self.buf_skip = 0;
+            self.buf_len = filled;
+        }
+        // Read from the buffer
+        let to_read = usize::min(buf.len(), self.buf_len - self.buf_skip);
+        eprintln!(
+            "reading encrypt buf {}..{} out of {}",
+            self.buf_skip,
+            self.buf_skip + to_read,
+            self.buf_len
+        );
+        buf[..to_read].copy_from_slice(&self.buf[self.buf_skip..self.buf_skip + to_read]);
+        self.buf_skip += to_read;
+        Ok(to_read)
+    }
+}
+
+pub struct AesDecrypt<R> {
+    inner: R,
+    cipher: Aes256Gcm,
+    nonce: u128,
+    last_chunk: bool,
+    buf: Box<[u8]>,
+    buf_len: usize,
+    buf_skip: usize,
+}
+impl<R: Read> AesDecrypt<R> {
+    pub fn new(pass: &[u8], mut inner: R) -> Result<Self> {
+        let mut salt = [0u8; 32];
+        inner
+            .read_exact(&mut salt)
+            .context("extract aes-gcm-1 salt")?;
+        let (cipher, nonce) = init_aes_cipher(pass, salt)?;
+
+        let mut buf = vec![0u8; usize::max(SALT_SIZE, CIPHERTEXT_CHUNK_SIZE)].into_boxed_slice();
+        buf[..SALT_SIZE].copy_from_slice(&salt[..]);
+        Ok(Self {
+            inner,
+            cipher,
+            nonce,
+            buf,
+            buf_len: 0,
+            buf_skip: 0,
+            last_chunk: false,
+        })
+    }
+
+    /// Expects a buffer of size at least `CIPHERTEXT_CHUNK_SIZE`.
+    fn fill_buf(&mut self) -> io::Result<usize> {
+        // Read plaintext into buffer
+        let mut filled = 0;
+        while filled < CIPHERTEXT_CHUNK_SIZE {
+            let n = self
+                .inner
+                .read(&mut self.buf[filled..CIPHERTEXT_CHUNK_SIZE])?;
+            if n == 0 {
+                break;
+            }
+            filled += n;
+        }
+        if filled < CIPHERTEXT_CHUNK_SIZE {
+            self.last_chunk = true;
+        }
+        if filled == 0 {
+            return Ok(0);
+        }
+        // Extract tag from buffer
+        if filled < AES_TAG_SIZE {
+            return Err(io::Error::new(io::ErrorKind::InvalidData, "unexpected eof"));
+        }
+        let mut tag = [0u8; AES_TAG_SIZE];
+        tag.copy_from_slice(&self.buf[..AES_TAG_SIZE]);
+        // Get next nonce
+        let mut nonce = [0u8; 12];
+        nonce.copy_from_slice(&self.nonce.to_le_bytes()[..12]);
+        self.nonce = self.nonce.saturating_add(1);
+        // Decrypt buffer
+        self.cipher
+            .decrypt_in_place_detached(
+                &nonce.into(),
+                &[],
+                &mut self.buf[AES_TAG_SIZE..filled],
+                &tag.into(),
+            )
+            .map_err(|_e| io::Error::new(io::ErrorKind::Other, "aes decryption error"))?;
+        Ok(filled)
+    }
+}
+impl<R> Read for AesDecrypt<R>
+where
+    R: Read,
+{
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        // Refill buffer if it is empty
+        if self.buf_skip >= self.buf_len {
+            // If we already reached the last chunk, short-circuit
+            if self.last_chunk {
+                return Ok(0);
+            }
+            // Fill the inner buffer
+            let filled = self.fill_buf()?;
+            if filled == 0 {
+                return Ok(0);
+            }
+            self.buf_skip = AES_TAG_SIZE;
+            self.buf_len = filled;
+        }
+        // Read from the buffer
+        let to_read = usize::min(buf.len(), self.buf_len - self.buf_skip);
+        buf[..to_read].copy_from_slice(&self.buf[self.buf_skip..self.buf_skip + to_read]);
+        self.buf_skip += to_read;
+        Ok(to_read)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::io::Read;
+
+    use super::{AesDecrypt, AesEncrypt, CIPHERTEXT_CHUNK_SIZE, PLAINTEXT_CHUNK_SIZE};
+
+    fn test_roundtrip(input: &[u8]) {
+        let enc = AesEncrypt::new(b"my password", input).unwrap();
+        let mut dec = AesDecrypt::new(b"my password", enc).unwrap();
+        let mut output = vec![];
+        dec.read_to_end(&mut output).unwrap();
+        assert_eq!(input, output);
+    }
+
+    #[test]
+    fn roundtrip_0() {
+        test_roundtrip(b"");
+    }
+
+    #[test]
+    fn roundtrip_1() {
+        test_roundtrip(b"x");
+    }
+
+    #[test]
+    fn roundtrip_few() {
+        test_roundtrip(b"hello world");
+    }
+
+    #[test]
+    fn roundtrip_chunk() {
+        let mut data = vec![0u8; CIPHERTEXT_CHUNK_SIZE];
+        getrandom::fill(&mut data).unwrap();
+        test_roundtrip(&data);
+    }
+
+    #[test]
+    fn roundtrip_plain_chunk() {
+        let mut data = vec![0u8; PLAINTEXT_CHUNK_SIZE];
+        getrandom::fill(&mut data).unwrap();
+        test_roundtrip(&data);
+    }
+
+    #[test]
+    fn roundtrip_chunk_offbyone() {
+        let mut data = vec![0u8; CIPHERTEXT_CHUNK_SIZE + 1];
+        getrandom::fill(&mut data).unwrap();
+        test_roundtrip(&data);
+        let mut data = vec![0u8; CIPHERTEXT_CHUNK_SIZE - 1];
+        getrandom::fill(&mut data).unwrap();
+        test_roundtrip(&data);
+    }
+
+    #[test]
+    fn roundtrip_plainchunk_offbyone() {
+        let mut data = vec![0u8; PLAINTEXT_CHUNK_SIZE + 1];
+        getrandom::fill(&mut data).unwrap();
+        test_roundtrip(&data);
+        let mut data = vec![0u8; PLAINTEXT_CHUNK_SIZE - 1];
+        getrandom::fill(&mut data).unwrap();
+        test_roundtrip(&data);
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,274 @@
+mod prelude {
+    pub use anyhow::{anyhow, bail, ensure, Context, Result};
+    pub use serde::{Deserialize, Serialize};
+    pub use std::{
+        fmt,
+        io::{self, BufReader, Read, Write},
+        path::PathBuf,
+        time::Duration,
+    };
+}
+
+use crate::prelude::*;
+use std::{
+    fs::File,
+    net::{SocketAddr, TcpStream},
+};
+
+use aes_gcm_1::{AesDecrypt, AesEncrypt, CIPHERTEXT_CHUNK_SIZE};
+use console::{style, Term};
+use flate2::{bufread::GzEncoder, read::GzDecoder};
+
+const PROTOCOL_VERSION: (u32, u32) = (1, 0);
+
+mod aes_gcm_1;
+
+enum Action {
+    Send,
+    Recv,
+}
+
+fn ask_char<T>(
+    mut term: &Term,
+    s: impl fmt::Display,
+    mut acc: impl FnMut(char) -> Option<T>,
+) -> Result<T> {
+    write!(term, "{} ", style(s).cyan().bold())?;
+    loop {
+        let c = term.read_char()?;
+        match acc(c) {
+            Some(t) => {
+                writeln!(term, "{}", c)?;
+                break Ok(t);
+            }
+            None => {}
+        }
+    }
+}
+
+fn ask_line(
+    mut term: &Term,
+    s: impl fmt::Display,
+    mut acc: impl FnMut(&str) -> bool,
+) -> Result<String> {
+    write!(term, "{}: ", style(s).cyan().bold())?;
+    loop {
+        let r = term.read_line()?;
+        if acc(&r) {
+            break Ok(r);
+        }
+    }
+}
+fn ask_secure_line(
+    mut term: &Term,
+    s: impl fmt::Display,
+    mut acc: impl FnMut(&str) -> bool,
+) -> Result<String> {
+    write!(term, "{}: ", style(s).cyan().bold())?;
+    loop {
+        let s = term.read_secure_line()?;
+        if acc(&s) {
+            break Ok(s);
+        }
+    }
+}
+
+pub fn main() -> Result<()> {
+    let term = Term::stdout();
+    let action = ask_char(
+        &term,
+        style("Do you want to send or receive? (S/R)").cyan(),
+        |c| match c {
+            's' | 'S' => Some(Action::Send),
+            'r' | 'R' => Some(Action::Recv),
+            _ => None,
+        },
+    )?;
+    match action {
+        Action::Send => {
+            send(term)?;
+        }
+        Action::Recv => {
+            recv(term)?;
+        }
+    }
+    Ok(())
+}
+
+#[derive(Serialize, Deserialize)]
+struct Header {
+    protocol: String,
+    version: (u32, u32),
+    filename: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(default)]
+    encryption: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(default)]
+    compression: Option<String>,
+}
+
+fn send_value<T: Serialize>(stream: &mut TcpStream, value: T) -> Result<()> {
+    let buf = serde_json::to_vec(&value)?;
+    let len = u16::try_from(buf.len()).context("json is too long")?;
+    let len_buf = len.to_le_bytes();
+    stream.write_all(&len_buf).context("network upload error")?;
+    stream.write_all(&buf).context("network upload error")?;
+    Ok(())
+}
+
+fn recv_value<T>(stream: &mut TcpStream) -> Result<T>
+where
+    T: for<'a> Deserialize<'a>,
+{
+    let mut len_buf = [0u8; 2];
+    stream
+        .read_exact(&mut len_buf)
+        .context("network download error")?;
+    let len = u16::from_le_bytes(len_buf);
+    let mut raw = vec![0u8; len as usize];
+    stream
+        .read_exact(&mut raw)
+        .context("network download error")?;
+    let val: T = serde_json::from_slice(&raw[..]).context("received malformed data")?;
+    Ok(val)
+}
+
+fn send(term: Term) -> Result<()> {
+    let path = PathBuf::from(ask_line(
+        &term,
+        "Enter the path of the file/directory to send",
+        |_| true,
+    )?);
+    let filename = path.file_name().ok_or_else(|| anyhow!("invalid path"))?;
+    let filename = filename
+        .to_str()
+        .ok_or_else(|| anyhow!("invalid utf-8 path"))?
+        .to_string();
+    let file = File::open(&path).context("open file")?;
+    let file = BufReader::new(file);
+
+    let pass = ask_secure_line(&term, "Enter password", |_| true)?;
+
+    let file = GzEncoder::new(file, flate2::Compression::fast());
+    let mut file = AesEncrypt::new(pass.as_bytes(), file)?;
+    let listener = std::net::TcpListener::bind("0.0.0.0:56273")?;
+    println!("Listening on post 56273...");
+    let (mut stream, addr) = listener.accept()?;
+    println!("Connection from {}, beaming over...", addr);
+    send_value(
+        &mut stream,
+        Header {
+            protocol: "dirsend".into(),
+            version: PROTOCOL_VERSION,
+            filename,
+            encryption: Some("aes-gcm-1".into()),
+            compression: Some("gzip".into()),
+        },
+    )?;
+    let mut buf = vec![0u8; CIPHERTEXT_CHUNK_SIZE];
+    loop {
+        let read_bytes = file.read(&mut buf[..]).context("file read error")?;
+        if read_bytes == 0 {
+            break;
+        }
+        stream
+            .write_all(&buf[..read_bytes])
+            .context("network upload error")?;
+    }
+    println!(
+        "{} Uploaded file at {}",
+        style("Success!").green().bold(),
+        path.display(),
+    );
+    Ok(())
+}
+
+fn recv(term: Term) -> Result<()> {
+    let addr = ask_line(&term, "Enter the address of the remote server", |_| true)?;
+    let addr: SocketAddr = addr.parse().context("invalid server address")?;
+    let mut stream = TcpStream::connect_timeout(&addr, Duration::from_secs(6))
+        .context("failed to connect to server")?;
+    let header: Header = recv_value(&mut stream)?;
+    ensure!(header.protocol == "dirsend", "invalid protocol");
+    ensure!(header.version.0 == 1, "incompatible protocol version");
+
+    let filename = header.filename.trim_ascii();
+    let banned = b"\0/\\";
+    ensure!(
+        !banned.iter().any(|ban| filename.as_bytes().contains(ban)),
+        "invalid filename"
+    );
+    ensure!(filename != "..", "invalid filename");
+    let mut path = PathBuf::from(filename);
+    ensure!(path.iter().count() == 1, "invalid filename");
+    ensure!(path.is_relative(), "invalid filename");
+    let mut counter = 1;
+    while path.exists() {
+        counter += 1;
+        let (stem, ext) = match filename.find('.') {
+            None | Some(0) => (filename, ""),
+            Some(pos) => filename.split_at(pos),
+        };
+        path = PathBuf::from(format!("{stem} ({counter}){ext}"));
+    }
+    if counter > 1 {
+        println!(
+            "{}",
+            style(format!(
+                "File {} already exists, downloading to {}",
+                filename,
+                path.display(),
+            ))
+            .yellow()
+        );
+    }
+
+    let mut stream: &mut dyn Read = &mut stream;
+    let mut decrypter;
+    match header.encryption.as_deref() {
+        None => {}
+        Some("aes-gcm-1") => {
+            println!("Contents encrypted with aes-gcm-1");
+            let pass = ask_secure_line(&term, "Enter password", |_| true)?;
+            decrypter = AesDecrypt::new(pass.as_bytes(), stream)?;
+            stream = &mut decrypter;
+        }
+        Some(unk) => {
+            bail!("unknown encryption mode '{unk}'")
+        }
+    }
+    let mut deflater;
+    match header.compression.as_deref() {
+        None => {}
+        Some("gzip") => {
+            println!("Using gzip compression");
+            deflater = GzDecoder::new(stream);
+            stream = &mut deflater;
+        }
+        Some(unk) => {
+            bail!("unknown compression mode '{unk}'")
+        }
+    }
+
+    let mut file = File::create_new(&path)?;
+    let mut buf = vec![0u8; CIPHERTEXT_CHUNK_SIZE];
+    loop {
+        let read_bytes = stream
+            .read(&mut buf[..])
+            .context("network download error")?;
+        if read_bytes == 0 {
+            break;
+        }
+        file.write_all(&buf[..read_bytes])
+            .context("file write error")?;
+    }
+
+    println!(
+        "{} Downloaded to {}",
+        style("Success!").green().bold(),
+        path.display(),
+    );
+
+    Ok(())
+}
